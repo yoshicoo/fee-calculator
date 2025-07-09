e7acde29acb66a8812fe73c0b570f25e324f58a8
yoshicoo <kosuke.yoshida.90@shiftinc.jp>
2025-07-09 14:48:09 +0900

    Create utils.ts

---
+++ lib/utils.ts
@@ -0,0 +1,20 @@
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function calculateFee(annualSalary: number, feeRate: number): number {
+  const fee = annualSalary * (feeRate / 100);
+  return Math.ceil(fee);
+}
+
+export function formatCurrency(amount: number): string {
+  return new Intl.NumberFormat('ja-JP', {
+    style: 'currency',
+    currency: 'JPY',
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+  }).format(amount);
+}
